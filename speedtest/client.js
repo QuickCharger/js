f558cca7a58ee40efc977fe2d63f98a33d4d42f8
bdg <bdgboss@126.com>
2020-05-04 01:42:55 +0800

    规范

--- speedtest/client.js
+++ speedtest/client.js
@@ -1,14 +1,42 @@
 const net = require('net');
 let easy = require('../lib/easy')
 
+// let process = [
+// 	{HostName:"127.0.0.1", Port:1234, SpeedSize = [1,1,1,1000000,1000000, 1000000]},
+// 	{HostName:"127.0.0.1", Port:2345, SpeedSize = [1,1,1,1000000,1000000, 1000000]},
+// 	{HostName:"127.0.0.1", Port:3456, SpeedSize = [1,1,1,1000000,1000000, 1000000]},
+// ]
+
+function speedLog(from, to, siz, ms) {
+	let speed = siz / ms
+	speed = easy.Fill(speed.toFixed(2), 8, " ")
+	if(siz < 1000) {
+		siz = easy.Fill(siz, 4, " ") + " B"
+	} else if(siz < 1000000) {
+		siz = easy.Fill(parseInt(siz/1000), 4, " ") + "KB"
+	} else if(siz < 1000000000) {
+		siz = easy.Fill(parseInt(siz/1000000), 4, " ") + "MB"
+	}
+
+	ms = easy.Fill(ms.toFixed(2), 8, " ")
+	console.log(`${from} => ${to}\t package size ${siz}   cost ${ms}ms   speed ${speed}KB/s`)
+}
+
 const hostname = '127.0.0.1';
 const portSpeed = 1001
 
 let speedCount = -1
 let speedSize = [
-	1, 1, 1,
-	1000000,			//1MB
-	50000000
+	0, 0, 0, 0, 0, 0,
+	100000,
+	100000,
+	100000,
+	1000000,
+	1000000,
+	1000000,
+	10000000,
+	10000000,
+	10000000,
 ]
 
 function TestSpeed() {
@@ -17,20 +45,22 @@ function TestSpeed() {
 	let bufRecv = new easy.TcpBufRecv
 	const socket = new net.Socket();
 	socket.setEncoding = 'UTF-8';
-	let t1 = 0
+	let tBegin = 0
 	let send = ''
 	socket.connect( portSpeed, hostname, function(){
 		send = easy.RandString(speedSize[speedCount])
-		send = new easy.TcpBufSend(send).Serialize()
-		t1 = process.uptime()
+		send = new easy.TcpBufSend(easy.MsgType.speed, send).Serialize()
+		tBegin = process.uptime()
+		// console.log(`count ${speedCount} send size ${send.length}`)
 		socket.write(send);
 	}).on( 'data', function ( msg ) {
 		let cur = process.uptime()
-		bufRecv.DeSerialize(msg, m => {
-			console.log( `${m}. send ${(send.length/1000).toFixed(2)}KB. cost ${(cur - t1).toFixed(3)}s. speed ${(send.length/(cur-t1)/1000).toFixed(3)}KB/s` )
+		// console.log(`get msg ${msg}`)
+		bufRecv.DeSerialize(msg, (type, msg) => {
+			speedLog("", "", send.length, (cur-tBegin)*1000)
 			socket.destroy()
-			setTimeout(TestSpeed, 300);
-		  })
+			setTimeout(TestSpeed, 100);
+		})
 	});
 }
 setTimeout(TestSpeed)
--- speedtest/server.js
+++ speedtest/server.js
@@ -7,19 +7,18 @@ const hostnameSpeed = '0.0.0.0'
 const server = new net.createServer();
 
 server.on('connection', (client) => {
-  let bufRecv = new easy.TcpBufRecv
-  let i = 0
-  client.on('data', function (msg) {
-    i += msg.length
-    console.log(`${msg.length}   ${i}`)
-    bufRecv.DeSerialize(msg, m => {
-      client.write(new easy.TcpBufSend(`over`).Serialize())
-    })
-  }).on('end', ()=> {
-  }).on('close',()=>{
-  }).on('error', e => {
-    console.log(e)
-  })
-}).listen( portSpeed,hostnameSpeed,function () {
-  console.log(`speed run ${hostnameSpeed}:${portSpeed}`);
+	let bufRecv = new easy.TcpBufRecv
+	let i = 0
+	client.on('data', (msg) => {
+		i += msg.length
+		bufRecv.DeSerialize(msg, (type, msg1) => {
+			client.write(new easy.TcpBufSend(easy.MsgType.over).Serialize())
+		})
+	}).on('end', () => {
+	}).on('close', () =>{
+	}).on('error', (e) => {
+		console.log(e)
+	})
+}).listen( portSpeed, hostnameSpeed, () => {
+	console.log(`speed run ${hostnameSpeed}:${portSpeed}`);
 });
