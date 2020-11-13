01f50ca2b841c53b06b9c02ddc1bb7d6b38110a1
bdg <bdgboss@126.com>
2020-11-13 16:12:27 +0800

    speedtest

--- speedtest.js
+++ speedtest.js
@@ -1,25 +1,82 @@
-const net = require('net');
+﻿const net = require('net');
 const Crypto = require('crypto')
 
 let argv = process.argv.splice(2)
-const SpeedSize = [0,0,10000,10000,100000,100000,1000000,1000000,10000000,10000000]
 const [MsgType_Speed,MsgType_Speed_Ret,MsgType_Info,MsgType_Info_Ret] = [1,2,3,4]	// MsgType_Speed 测试数据包  MsgType_Speed_Ret 测试数据包返回  MsgType_Info 将要测试的地址等信息
-let [defaultPort, packInterval] = [1234, 500]
-let steps = {
-	host:"127.0.0.1", port:1234, SpeedSize:[0,0,0], next:{ 
-		host:"127.0.0.1", port:1234, SpeedSize:[0,0, 1000000, 1000000], next:{
-			host:"127.0.0.1", port:8883, SpeedSize:[0,0, 1000000, 1000000], next:{
-				host:"127.0.0.1", port:1234}}}}
-
-let fSpeedLog = (from, to, siz, sec) => `${from} => ${to.host}:${to.port}\t package size ${(siz<1000 ? siz : siz < 1000000 ? siz / 1000 : siz / 1000000).toFixed().toString().padStart(4) + (siz<1000 ? " B" : siz < 1000000 ? "KB" : "MB")}   cost ${(sec*1000).toFixed(2).padStart(8)}ms   speed ${(siz / sec / 1000).toFixed(2).padStart(8)}KB/s`
-let fRandString_Fast = (a_Length) => Crypto.randomBytes(a_Length*0.75 + 0.25).toString('base64').slice(0, a_Length)
+let [defaultPort, packInterval, SpeedSize] = [12345, 500, [0,10000,1000000]]
+const strRand = (a_Length => Crypto.randomBytes(a_Length*0.75 + 0.25).toString('base64').slice(0, a_Length))(10000000)
+const steps = [
+	{host:"127.0.0.1", SpeedSize:[], next:[
+		{host:"127.0.0.1", SpeedSize:[0,0,0,10000,10000,10000,1000000,1000000,1000000]},
+	]},
+]
 
 // init
 if(argv[0]) {
 	defaultPort = argv[0]
-	steps = {}
 }
-let strRand = fRandString_Fast(10000000)
+
+let fSpeedLog = (from, to, siz, sec) => `${from.host}:${from.port} => ${to.host}:${to.port}\t package size ${(siz<1000 ? siz : siz < 1000000 ? siz / 1000 : siz / 1000000).toFixed().toString().padStart(4) + (siz<1000 ? " B" : siz < 1000000 ? "KB" : "MB")}   cost ${(sec*1000).toFixed(2).padStart(8)}ms   speed ${(siz / sec / 1000).toFixed(2).padStart(8)}KB/s`
+
+let fDoSpeedTest = async (info = steps, previousSocket = null) => {
+	return new Promise(async (resolve, reject) => {
+		if(!info || info.length == 0) {
+			previousSocket && previousSocket.end()
+			return resolve(null)
+		}
+		for(let step of info) {
+			let spdInfo = step.SpeedSize || SpeedSize
+			for(let packSize of spdInfo) {
+				let param = {siz:packSize, from:{host:previousSocket?previousSocket.localAddress:"this", port:previousSocket?previousSocket.localPort:0}, to:{host:step.host, port:step.port||defaultPort}}
+				try{
+					let ret = await wait_sync(packInterval, TestSpeed, param)
+					previousSocket && previousSocket.write(fSerialize(MsgType_Info_Ret, ret))
+					console.log(ret)
+				} catch(e) {
+					console.log(`do ${JSON.stringify(param)} failed. reason ${e}`)
+					previousSocket && previousSocket.end(fSerialize(MsgType_Info_Ret, e))
+					break
+				}
+			}
+			if(step.next && step.next.length > 0) {
+				await TransNextInfo(step, previousSocket)
+			}
+		}
+		previousSocket && previousSocket.end()
+		resolve(null)
+	})
+}
+
+setTimeout(fDoSpeedTest, 1000);
+
+(function fCreateServer() {
+	net.createServer().on('connection', (remote) => {
+		let bufRecv = new TcpBufRecv
+		let i = 0
+		//console.log(`localAddress ${remote.localAddress} localPort ${remote.localPort}  remoteAddress ${remote.remoteAddress} remotePort ${remote.remotePort}`)
+		remote.on('data', (msg) => {
+			i += msg.length
+			bufRecv.DeSerialize(msg, async (type, msg) => {
+				if(type == MsgType_Speed) {
+					let c = fSerialize(MsgType_Speed_Ret)
+					remote.write(c, 'utf8')
+				} else if(type == MsgType_Info) {
+					let c = JSON.parse(msg)
+					console.log(`do ${msg}`)
+					await fDoSpeedTest(c, remote)
+				}
+			})
+		}).on('error', (e) => {
+			console.log(e)
+		})
+	}).listen( port=defaultPort, "0.0.0.0", () => {
+		console.log(`listen port ${defaultPort} success`);
+	}).on("error", (err) => {
+		console.log(`listen port ${defaultPort} failed`)
+		setTimeout(fCreateServer, 10000)
+	})
+})();
+
 
 class TcpBufRecv {
 	constructor() {
@@ -59,7 +116,6 @@ let fSerialize = (type, context = '') => {
 	return Buffer.concat([bufencode, Buffer.from(context, "binary")])
 }
 
-// 写成函数 传入配置
 function TestSpeed({from, to, siz}) {
 	return new Promise((resolve, reject) => {
 		let bufRecv = new TcpBufRecv
@@ -83,11 +139,27 @@ function TestSpeed({from, to, siz}) {
 	})
 }
 
+function wait_sync(ms, cb, param) {
+	return new Promise((resolve, reject) => {
+		setTimeout(async ()=>{
+			try{
+				let ret = await cb(param)
+				resolve(ret)
+			} catch (e) {
+				reject(e)
+			}
+		}, ms)
+	})
+}
+
 function TransNextInfo(info, previousSocket = null) {
 	return new Promise((resolve, reject)=>{
 		let bufRecv = new TcpBufRecv
+		let host = info.host
+		let port = info.port || defaultPort
 		const socket = new net.Socket().setEncoding('utf8');
-		socket.connect(info.port, info.host, ()=>{
+		console.log(`TransNextInfo to ${host}:${port}`)
+		socket.connect(port, host, ()=>{
 			socket.write(fSerialize(MsgType_Info, JSON.stringify(info.next)))
 		}).on('data', (msg)=> {
 			bufRecv.DeSerialize(msg, (type, msg) => {
@@ -107,74 +179,3 @@ function TransNextInfo(info, previousSocket = null) {
 		})
 	})
 }
-
-function wait_sync(ms, cb, param) {
-	return new Promise((resolve, reject) => {
-		setTimeout(async ()=>{
-			try{
-				let ret = await cb(param)
-				resolve(ret)
-			} catch (e) {
-				reject(e)
-			}
-		}, ms)
-	})
-}
-
-let fDoSpeedTest = async (info = steps, previousSocket = null) => {
-	return new Promise(async (resolve, reject) => {
-		if(!info || !info.host) {
-			previousSocket && previousSocket.end()
-			return resolve(null)
-		}
-		let spdInfo = info.SpeedSize || SpeedSize
-		for(let it of spdInfo) {
-			let param = {siz:it, from:info.from || "this", to:{host:info.host, port:info.port||defaultPort}}
-			try{
-				let ret = await wait_sync(packInterval, TestSpeed, param)
-				previousSocket && previousSocket.write(fSerialize(MsgType_Info_Ret, ret))
-				console.log(ret)
-			} catch(e) {
-				console.log(`do ${JSON.stringify(param)} failed. reason ${e}`)
-				previousSocket && previousSocket.end(fSerialize(MsgType_Info_Ret, e))
-				return
-			}
-		}
-		if(info.next) {
-			info.next.from = `${info.host}:${info.port}`
-			await TransNextInfo(info, previousSocket)
-		}
-		previousSocket && previousSocket.end()
-		resolve(null)
-	})
-}
-
-setTimeout(fDoSpeedTest, 1000);
-
-
-(function fCreateServer() {
-	net.createServer().on('connection', (remote) => {
-		let bufRecv = new TcpBufRecv
-		let i = 0
-		remote.on('data', (msg) => {
-			i += msg.length
-			bufRecv.DeSerialize(msg, async (type, msg) => {
-				if(type == MsgType_Speed) {
-					let c = fSerialize(MsgType_Speed_Ret)
-					remote.write(c, 'utf8')
-				} else if(type == MsgType_Info) {
-					let c = JSON.parse(msg)
-					console.log(`do ${msg}`)
-					await fDoSpeedTest(c, remote)
-				}
-			})
-		}).on('error', (e) => {
-			console.log(e)
-		})
-	}).listen( port=defaultPort, "0.0.0.0", () => {
-		console.log(`listen port ${defaultPort} success`);
-	}).on("error", (err) => {
-		console.log(`listen port ${defaultPort} failed`)
-		setTimeout(fCreateServer, 10000)
-	})
-})();
