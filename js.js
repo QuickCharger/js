aa317505c4bb3dc024f16dec341bba56a8e6795e
bdg <bdgboss@126.com>
2020-11-18 15:24:14 +0800

    1

--- js.js
+++ js.js
@@ -37,3 +37,13 @@ await执行步骤（不论后面的表达式是否是Promise）
 	3. Promise执行了resolve后 从await处继续执行。
 		如果Promise一直不执行resolve，则一直不会执行await后面的代码
 		如果await后面不是Promise，则执行完2后 立即执行3（不等待Promise执行resolve）
+
+					end方法		end事件		close方法	close事件	error事件
+客户端	client        ok          ok                        ok          ok     
+服务器	comein  同客户端的client
+服务器	server                                  ok          ok          ok     
+
+client end方法 执行后会发送FIN包，comein会收到 1.end事件 2.close事件
+client 意外中断后，comein会收到 1.error事件 2.close事件
+server close方法 主动调用后，不再接收新的comein，老的comein会保持
+       close事件 在所有的comein都被关闭后会被触发
